da682d28f94817be67b0dbff727400a3d947ee43
kozmarti <kozmarti@gmail.com>
2021-02-03 11:33:01 +0100

    js css sass bootstrap jquery fontawesome installed in project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,19 @@
 
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
-class HomeController
+class HomeController extends AbstractController
+
 {
-
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
 }
