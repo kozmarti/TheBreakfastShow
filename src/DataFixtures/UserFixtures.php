b2ff95d1e2395340e6a822a70c3b6874a1e1a67c
kozmarti <kozmarti@gmail.com>
2021-02-04 14:46:00 +0100

    admin login added and user fixtures for admin

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use     App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $admin = new User();
+        $admin->setUsername('Rencsi');
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'password'
+        ));
+        $admin->setRoles([User::ADMIN]);
+        $manager->persist($admin);
+
+
+        $manager->flush();
+    }
+}
