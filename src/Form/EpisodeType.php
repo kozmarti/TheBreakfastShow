c7d3b41b1bdaed1f9bf2a4df1f759d0c82580fc5
kozmarti <kozmarti@gmail.com>
2021-02-04 09:09:46 +0100

    welcome page done, CRUD in progress

---
+++ src/Form/EpisodeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Episode;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EpisodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('number')
+            ->add('recipename')
+            ->add('preparationtime')
+            ->add('person')
+            ->add('preparation')
+            ->add('season')
+            ->add('images')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Episode::class,
+        ]);
+    }
+}
