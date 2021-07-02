af2bd7365409f9a3d45ca1e1c4ac53834cf9f825
kozmarti <kozmarti@gmail.com>
2021-07-02 09:53:31 +0200

    unique added

--- src/Entity/Episode.php
+++ src/Entity/Episode.php
@@ -6,9 +6,11 @@ use App\Repository\EpisodeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=EpisodeRepository::class)
+ * @UniqueEntity(fields={"title"}, message="There is already a recipe with this episode name")
  */
 class Episode
 {
