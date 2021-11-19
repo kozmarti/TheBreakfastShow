70ef685fac6637f96d011ed7c7f6119ea2f0715a
kozmarti <kozmarti@gmail.com>
2021-11-19 21:10:24 +0100

    updated...

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,7 +1,29 @@
+let imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+[].forEach.call(imgs, function(img) {
+    if (img.complete)
+        incrementCounter();
+    else
+        img.addEventListener('load', incrementCounter, false);
+});
+
+function incrementCounter() {
+    counter++;
+    if (counter === len) {
+        let toHide = document.getElementById("spinner-to-hide")
+        let toShow = document.getElementById("content-to-show")
+        toShow.classList.remove("d-none");
+        toShow.classList.add("fade-in-effect");
+        toHide.classList.add("d-none");
+    }
+}
+
 let $heart = document.getElementsByClassName("fa-heart")[0];
 console.log($heart);
 
-$heart.addEventListener('click', (event)=>    {
+$heart.addEventListener('click', (event) => {
     $splitData = event.target.id.split('-');
     console.log($splitData);
     if (event.target.classList.contains('fas')) {
@@ -9,10 +31,10 @@ $heart.addEventListener('click', (event)=>    {
         event.target.classList.add('far');
 
 
-    }  else {
+    } else {
         event.target.classList.remove('far');
         event.target.classList.add('fas');
     }
     fetch('/favorites?user-id=' + $splitData[0] + '&episode-id=' + $splitData[1])
-        .then( res=>res.json())
+        .then(res => res.json())
 });
